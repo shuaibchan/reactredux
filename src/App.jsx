f65f6ccb0411f6c833b0253955da10d03eeda7ed
Shuaib Bin Chantando <shuaib@cybersecurity.my>
2018-01-05 03:23:05 +0800

    sampai tutorial no 016 Creating a Clock Component

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { Component }from 'react';
+import './App.css';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      deadline: 'December 25,2017',
+      newDeadline: ''
+    }
+  }
+
+  changeDeadLine(){
+  console.log('state',this.state);
+    this.setState({deadline: this.state.newDeadline})
+  }
+  render(){
+  return(
+    <div className="App">
+      <div className="App-titles">Countdown to {this.state.deadline}</div>
+      <div>
+        <div className="Clock-days">14 Days</div>
+        <div className="Clock-hours">30 Hours</div>
+        <div className="Clock-minutes">15 Minutes</div>
+        <div className="Clock-seconds">20 Seconds</div>
+      </div>
+      <div>
+        <input placeholder='new date'
+        onChange={event => this.setState({newDeadline:event.target.value})}
+        />
+        <button onClick={() => this.changeDeadLine()}>
+          Submit
+        </button>
+      </div>
+    </div>
+    )
+  }
+}
+export default App;
